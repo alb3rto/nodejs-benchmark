7c6e78d27146a08c25ac0f516ca0421c68dd290f
Alberto Sanchez <asanchez@talpor.com>
2014-03-26 17:13:16 -0430

    base server

---
+++ static/js/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var os = require('os');
+
+http.createServer(function (req, res) {
+    console.log('Request:', req);
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('success\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
